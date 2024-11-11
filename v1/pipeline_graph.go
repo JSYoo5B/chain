456e5de2c60bda5436660a108e2e42f4f88cdc54
JaeSang Yoo <jsyoo5b@gmail.com>
2024-11-12 01:05:50 +0900

    feat: make ValidateGraph to print cycle

--- v1/pipeline_graph.go
+++ v1/pipeline_graph.go
@@ -8,15 +8,15 @@ import "fmt"
 // If the union of all visited nodes equals the total number of nodes, the graph is connected and the check finishes.
 func (p *Pipeline[T]) ValidateGraph() error {
 	// Step 1: Perform DFS from initAction to check for cycles and track visited nodes
-	visited := make(map[Action[T]]bool)
-	if err := p.dfsWithCycleCheck(p.initAction, visited); err != nil {
+	visited := make(map[Action[T]]int)
+	if err := p.dfsWithCycleCheck(p.initAction, visited, []string{}); err != nil {
 		return err
 	}
 
 	// Step 2: After DFS, check if all actions have been visited
 	unvisited := make([]Action[T], 0, len(p.runPlans))
 	for action := range p.runPlans {
-		if !visited[action] {
+		if visited[action] == notVisited {
 			unvisited = append(unvisited, action)
 		}
 	}
@@ -24,8 +24,8 @@ func (p *Pipeline[T]) ValidateGraph() error {
 	// Step 3: If there are unvisited nodes, start new DFS from them
 	for len(unvisited) > 0 {
 		newStart := unvisited[0] // Pick any unvisited node
-		visitedFromNewStart := make(map[Action[T]]bool)
-		if err := p.dfsWithCycleCheck(newStart, visitedFromNewStart); err != nil {
+		visitedFromNewStart := make(map[Action[T]]int)
+		if err := p.dfsWithCycleCheck(newStart, visitedFromNewStart, []string{}); err != nil {
 			return err
 		}
 
@@ -34,10 +34,10 @@ func (p *Pipeline[T]) ValidateGraph() error {
 		// If there is no intersection, it's a disconnected graph.
 		intersectionFound := false
 		for action := range visitedFromNewStart {
-			if visited[action] {
+			if visited[action] != notVisited {
 				intersectionFound = true
 			}
-			visited[action] = true
+			visited[action] = confirmed
 		}
 
 		// Step 5: If no intersection found, it means the graph is disconnected
@@ -45,15 +45,15 @@ func (p *Pipeline[T]) ValidateGraph() error {
 			return fmt.Errorf("disconnect detected: action `%s` cannot reach the graph started from initAction `%s`", newStart.Name(), p.initAction.Name())
 		}
 
-		// Check if the union of visited nodes equals the total number of actions
+		// Step 6: Check all nodes have been visited, no need for further checks
 		if len(visited) == len(p.runPlans) {
-			return nil // All nodes have been visited, no need for further checks
+			return nil
 		}
 
 		// If there are still unvisited nodes, continue to the next round of DFS
 		stillUnvisited := make([]Action[T], 0, len(unvisited))
 		for _, action := range unvisited {
-			if !visited[action] {
+			if visited[action] == notVisited {
 				stillUnvisited = append(stillUnvisited, action)
 			}
 		}
@@ -63,22 +63,34 @@ func (p *Pipeline[T]) ValidateGraph() error {
 	return nil
 }
 
-func (p *Pipeline[T]) dfsWithCycleCheck(current Action[T], visited map[Action[T]]bool) error {
-	if visited[current] {
-		return fmt.Errorf("cycle detected: action `%s` has been visited twice", current.Name())
+func (p *Pipeline[T]) dfsWithCycleCheck(node Action[T], visited map[Action[T]]int, path []string) error {
+	path = append(path, "`"+node.Name()+"`")
+
+	if visited[node] != notVisited {
+		return fmt.Errorf("cycle detected: %v", path)
 	}
 
-	visited[current] = true
+	visited[node] = visiting
 
-	// Get the next actions based on current action's direction
-	plan := p.runPlans[current]
-	for _, nextAction := range plan {
-		if nextAction != nil {
-			if err := p.dfsWithCycleCheck(nextAction, visited); err != nil {
+	terminate := Terminate[T]()
+	for direction, nextAction := range p.runPlans[node] {
+		if nextAction != terminate {
+			edge := "-" + direction + "->"
+			path = append(path, edge)
+			if err := p.dfsWithCycleCheck(nextAction, visited, path); err != nil {
 				return err
 			}
+			path = path[:len(path)-1]
 		}
 	}
 
+	visited[node] = confirmed
+
 	return nil
 }
+
+const (
+	notVisited = iota
+	visiting
+	confirmed
+)
