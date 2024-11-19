8751a5292030750f00687fe26540885aaa7c2112
JaeSang Yoo <jsyoo5b@gmail.com>
2024-11-20 01:19:30 +0900

    docs: Add explanation for AggregateAction tests

--- railway/internal/example_aggregate_test.go
+++ railway/internal/example_aggregate_test.go
@@ -8,22 +8,69 @@ import (
 	"testing"
 )
 
+// The following functions create two types of actions, Action[int] and Action[string],
+// where the generic types are different, making it impossible to handle them within a single pipeline.
+
+func newIncAction(name string) railway.Action[int] {
+	runFunc := func(_ context.Context, input int) (output int, direction string, err error) {
+		return input + 1, railway.Success, nil
+	}
+	return railway.NewSimpleAction(name, runFunc)
+}
+
+func newAppendAction(name string) railway.Action[string] {
+	runFunc := func(_ context.Context, input string) (output string, direction string, err error) {
+		return input + "o", railway.Success, nil
+	}
+	return railway.NewSimpleAction(name, runFunc)
+}
+
+// By aggregating int and string into a single `Wrap` struct,
+// new actions are defined for `Action[Wrap]` which combine each `Action[int]` and `Action[string]`.
+// This enables handling different types of actions within a single pipeline.
+
+type Wrap struct {
+	number  int
+	message string
+}
+
+func newNumberAction(action railway.Action[int]) railway.Action[Wrap] {
+	getter := func(c Wrap) int { return c.number }
+	setter := func(c Wrap, i int) Wrap {
+		c.number = i
+		return c
+	}
+	return railway.NewAggregateAction(action, getter, setter)
+}
+
+func newMessageAction(action railway.Action[string]) railway.Action[Wrap] {
+	getter := func(c Wrap) string { return c.message }
+	setter := func(c Wrap, s string) Wrap {
+		c.message = s
+		return c
+	}
+	return railway.NewAggregateAction(action, getter, setter)
+}
+
+// This test demonstrates how different types of actions can be handled within a single pipeline.
+// It shows that both simple Action and Pipeline (treated as Action) can be combined and processed together.
+
 func TestAggregatePipeline(t *testing.T) {
 	logrus.SetLevel(logrus.DebugLevel)
 
 	t.Run("simple aggregate pipeline", func(t *testing.T) {
-		compositePipeline := railway.NewPipeline(
-			"CompositeTest",
-			NewNumberAction(&IncAction{"action1"}),
-			NewMessageAction(&GreetAction{"action2"}),
-			NewNumberAction(&IncAction{"action3"}),
-			NewNumberAction(&IncAction{"action4"}),
-			NewMessageAction(&GreetAction{"action5"}),
+		aggregatePipeline := railway.NewPipeline(
+			"ActionAggregateTest",
+			newNumberAction(newIncAction("action1")),
+			newMessageAction(newAppendAction("action2")),
+			newNumberAction(newIncAction("action3")),
+			newNumberAction(newIncAction("action4")),
+			newMessageAction(newAppendAction("action5")),
 		)
 
-		input := Aggregate{number: 10, message: "f"}
+		input := Wrap{number: 10, message: "f"}
 		// {10, f} -> {11, f} -> {11, fo} -> {12, fo} -> {13, fo} -> {13, foo}
-		output, direction, err := compositePipeline.Run(context.Background(), input)
+		output, direction, err := aggregatePipeline.Run(context.Background(), input)
 
 		assert.NoError(t, err)
 		assert.Equal(t, railway.Success, direction)
@@ -31,69 +78,42 @@ func TestAggregatePipeline(t *testing.T) {
 		assert.Equal(t, "foo", output.message)
 	})
 
-	t.Run("complex aggregate pipeline", func(t *testing.T) {
-		inc2Action := railway.NewPipeline("Inc2Action", &IncAction{"inc1"}, &IncAction{"inc2"})
-		greetAction := railway.NewPipeline("GreetAction", &GreetAction{}, &GreetAction{})
-		inc5Action := railway.NewPipeline("Inc5Action", &IncAction{"inc3"}, &IncAction{"inc4"}, &IncAction{"inc5"}, &IncAction{"inc"}, &IncAction{"inc7"})
-
-		compositePipeline := railway.NewPipeline("CompositeTest",
-			NewNumberAction(inc2Action),
-			NewMessageAction(greetAction),
-			NewNumberAction(inc5Action),
+	t.Run("nested pipeline aggregate pipeline", func(t *testing.T) {
+		inc2Action := railway.NewPipeline(
+			"Inc2Action",
+			newIncAction("inc1"),
+			newIncAction("inc2"),
+		)
+		append2Action := railway.NewPipeline(
+			"Append2Action",
+			newAppendAction("append1"),
+			newAppendAction("append2"),
+		)
+		inc5Action := railway.NewPipeline(
+			"Inc5Action",
+			newIncAction("inc3"),
+			newIncAction("inc4"),
+			newIncAction("inc5"),
+			newIncAction("inc6"),
+			newIncAction("inc7"),
 		)
 
-		input := Aggregate{number: 10, message: "f"}
+		aggregatePipeline := railway.NewPipeline(
+			"PipelineAggregateTest",
+			newNumberAction(inc2Action),
+			newMessageAction(append2Action),
+			newNumberAction(inc5Action),
+		)
+
+		input := Wrap{number: 10, message: "f"}
 		// {10, f} -> {11, f} -> {12, f}
 		// -> {12, fo} -> {12, foo}
 		// -> {13, foo} -> {14, foo} -> {15, foo} -> {16, foo} -> {17, foo}
-		output, direction, err := compositePipeline.Run(context.Background(), input)
+		output, direction, err := aggregatePipeline.Run(context.Background(), input)
 
 		assert.NoError(t, err)
 		assert.Equal(t, railway.Success, direction)
 		assert.Equal(t, 17, output.number)
 		assert.Equal(t, "foo", output.message)
 	})
-}
-
-type Aggregate struct {
-	number  int
-	message string
-}
-
-func NewNumberAction(action railway.Action[int]) railway.Action[Aggregate] {
-	getter := func(c Aggregate) int { return c.number }
-	setter := func(c Aggregate, i int) Aggregate {
-		c.number = i
-		return c
-	}
-	return railway.NewAggregateAction(action, getter, setter)
-}
-
-func NewMessageAction(action railway.Action[string]) railway.Action[Aggregate] {
-	getter := func(c Aggregate) string { return c.message }
-	setter := func(c Aggregate, s string) Aggregate {
-		c.message = s
-		return c
-	}
-	return railway.NewAggregateAction(action, getter, setter)
-}
-
-type IncAction struct{ name string }
-
-func (i IncAction) Name() string { return i.name }
-func (IncAction) Directions() []string {
-	return []string{railway.Success, railway.Error, railway.Abort}
-}
-func (IncAction) Run(_ context.Context, input int) (output int, direction string, err error) {
-	return input + 1, railway.Success, nil
-}
-
-type GreetAction struct{ name string }
-
-func (g GreetAction) Name() string { return g.name }
-func (GreetAction) Directions() []string {
-	return []string{railway.Success, railway.Error, railway.Abort}
-}
-func (GreetAction) Run(_ context.Context, input string) (output string, direction string, err error) {
-	return input + "o", railway.Success, nil
 }
