c9834d833672411f17cb15ce04c4d3766dcb373c
JaeSang Yoo <jsyoo5b@gmail.com>
2025-08-02 03:14:14 +0900

    feat: Add ParallelMapPipeline and ParallelSlicePipeline
    - Introduce NewParallelMapPipeline and NewParallelSlicePipeline
    - Process map or slice in parallel
    - When panic occurs, it does not affect other parallel executions

---
+++ parallel_slice_pipeline.go
@@ -0,0 +1,71 @@
+package chain
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/JSYoo5B/chain/internal/logger"
+	"runtime/debug"
+	"sync"
+)
+
+func NewParallelSlicePipeline[T any](name string, action Action[T]) Action[[]T] {
+	return &parallelSlicePipeline[T]{
+		name:   name,
+		action: action,
+	}
+}
+
+type parallelSlicePipeline[T any] struct {
+	name   string
+	action Action[T]
+}
+
+func (p parallelSlicePipeline[T]) Name() string { return p.name }
+func (p parallelSlicePipeline[T]) Run(ctx context.Context, input []T) (output []T, err error) {
+	pCtx := logger.WithRunnerDepth(ctx, p.name)
+	output = make([]T, len(input))
+	copy(output, input)
+
+	wg := sync.WaitGroup{}
+	wg.Add(len(input))
+	runIndex := func(i int, in T) {
+		// Wrap panic handling for safe running in a pipeline
+		defer func() {
+			if panicErr := recover(); panicErr != nil {
+				logger.Errorf(pCtx, "chain: panic occurred on running index %d, caused by %v", i, panicErr)
+				debug.PrintStack()
+
+				switch x := panicErr.(type) {
+				case string:
+					err = errors.New(x)
+				case error:
+					err = x
+				default:
+					err = errors.New("unknown panic type")
+				}
+				output[i] = in
+				wg.Done()
+				return
+			}
+		}()
+
+		logger.Debugf(pCtx, "chain: running index %d", i)
+
+		c := logger.WithRunnerDepth(ctx, fmt.Sprintf("%s[%d]/%s", p.name, i, p.action.Name()))
+
+		out, e := p.action.Run(c, in)
+		if e != nil {
+			logger.Errorf(pCtx, "chain: error occurred in index %d: %v", i, e)
+			err = e
+		}
+		output[i] = out
+		wg.Done()
+	}
+	for i, in := range input {
+		go runIndex(i, in)
+	}
+	wg.Wait()
+
+	return output, err
+}
