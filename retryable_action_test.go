7c0e5db682276bc1b80cc785c24955caae93722d
JaeSang Yoo <jsyoo5b@gmail.com>
2025-08-27 22:06:57 +0900

    feat: Add retryable action

---
+++ retryable_action_test.go
@@ -0,0 +1,56 @@
+package chain
+
+import (
+	"context"
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestRetryableAction(t *testing.T) {
+	logrus.SetLevel(logrus.DebugLevel)
+
+	checkZero := NewSimpleAction(
+		"checkZero",
+		func(ctx context.Context, input int) (int, error) {
+			if input == 0 {
+				return input, nil
+			}
+			return input, fmt.Errorf("%d is not zero", input)
+		})
+	decrease := NewSimpleAction(
+		"decrase",
+		func(ctx context.Context, input int) (int, error) {
+			return input - 1, nil
+		})
+	expectZero := NewRetryableAction("expectZero", checkZero, decrease, 3)
+
+	t.Run("direct success", func(t *testing.T) {
+		output, err := expectZero.Run(context.Background(), 0)
+
+		assert.NoError(t, err)
+		assert.Equal(t, 0, output)
+	})
+
+	t.Run("first retry success", func(t *testing.T) {
+		output, err := expectZero.Run(context.Background(), 1)
+
+		assert.NoError(t, err)
+		assert.Equal(t, 0, output)
+	})
+
+	t.Run("max retry success", func(t *testing.T) {
+		output, err := expectZero.Run(context.Background(), 2)
+
+		assert.NoError(t, err)
+		assert.Equal(t, 0, output)
+	})
+
+	t.Run("max retry fail", func(t *testing.T) {
+		output, err := expectZero.Run(context.Background(), 10)
+
+		assert.Error(t, err)
+		assert.NotEqual(t, 0, output)
+	})
+}
